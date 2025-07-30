ed9f6fbe44cda4336d762a5cd5a2268f55187227
Илиан Чолпанов <iliancholpanov@gmail.com>
2025-07-30 12:48:39 +0300

    Add starter UI components and update layout
    Introduces a comprehensive set of starter UI components for cabins, reservations, navigation, and user profile management. Updates the main layout to include a new Logo and Navigation, removes old SVG assets, and adds new images and styles. Also includes placeholder data and pages for cabins, about, login, and profile, setting up the foundation for a luxury cabin booking application.

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -4,8 +4,6 @@ import Navigation from "../components/Navigation";
 export default function Page() {
   return (
     <div>
-      <Navigation />
-
       <h1>Cabins page</h1>
     </div>
   );
--- app/page.js
+++ app/page.js
@@ -4,8 +4,6 @@ import Navigation from "./components/Navigation";
 export default function Page() {
   return (
     <div>
-      <Navigation />
-
       <h1>Evernoth Lodge. Welcome to the nordic paradise</h1>
 
       <Link href="/cabins">Explore luxury cabins</Link>
