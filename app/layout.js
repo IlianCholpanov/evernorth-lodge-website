1ed2cd9fd6c9ff86cf5b0a474f10da5516994069
Илиан Чолпанов <iliancholpanov@gmail.com>
2025-08-14 17:24:55 +0300

    add state management between component with Context API

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "@/app/_styles/globals.css";
 import Header from "./_components/Header";
 
 import { Josefin_Sans } from "next/font/google";
+import { ReservationProvider } from "./_components/ReservationContext";
 
 const josefin = Josefin_Sans({
   subsets: ["latin"],
@@ -25,8 +26,11 @@ export default function RootLayout({ children }) {
         className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
       >
         <Header />
+
         <div className="flex-1 px-8 py-12 grid">
-          <main className="max-w-7xl mx-auto w-full">{children}</main>
+          <main className="max-w-7xl mx-auto w-full">
+            <ReservationProvider>{children}</ReservationProvider>
+          </main>
         </div>
       </body>
     </html>
